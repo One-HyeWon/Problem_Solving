a6eb9e629990dcd70faeb6db548176a032a7c470
HyeWon Cho <5876675@gmail.com>
2023-04-27 17:55:36 +0900

    [1차] 비밀지도

---
+++ Programmers/Lv.1/[1차]_비밀지도.js
@@ -0,0 +1,45 @@
+function solution(n, arr1, arr2) {
+  const map1 = arr1;
+  const map2 = arr2;
+  const size = n;
+  const answer = [];
+
+  for (let i = 0; i < size; i++) {
+    let num1 = map1[i].toString(2);
+    let num2 = map2[i].toString(2);
+    const numArr = [];
+    let joinArr = '';
+
+    const num1Length = num1.length;
+    const num2Length = num2.length;
+
+    if (num1.length !== size) {
+      for (let i = 0; i < size - num1Length; i++) {
+        const temp = '0' + num1;
+        num1 = temp;
+      }
+    }
+
+    if (num2.length !== size) {
+      for (let i = 0; i < size - num2Length; i++) {
+        const temp = '0' + num2;
+        num2 = temp;
+      }
+    }
+    console.log('num1, num2 == ' + num1 + ' ' + num2);
+
+    for (let j = 0; j < size; j++) {
+      if (num1[j] === '0' && num2[j] === '0' && num1[j] === num2[j]) {
+        numArr.push(' ');
+      } else {
+        numArr.push('#');
+      }
+    }
+    joinArr = numArr.join('');
+    answer.push(joinArr);
+  }
+
+  return answer;
+}
+
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
