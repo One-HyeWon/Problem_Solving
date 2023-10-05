e71fb07099919a9929425d614d171b2c585d7f69
One-HyeWon <5876675@naver.com>
2023-10-05 12:10:02 +0900

    Lv2. 다음 큰 숫자

---
+++ Programmers/Lv.2/다음_큰_숫자.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    
+    function nNum(number) {
+        return number.toString(2).split("").filter(num => num == 1);
+    }
+    
+    const oneNum = nNum(n);
+    
+    for (let i = n+1 ; i < 1000000 ; i++) {
+            if (nNum(i).length == oneNum.length) return i;  
+    }
+    
+} 
